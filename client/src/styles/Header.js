14c345691455ec253844c976c609c913acd47130
lavalbuena357 <cuervo357@gmail.com>
2022-03-23 12:19:45 -0500

    styled components header

---
+++ client/src/styles/Header.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const Title = styled.h1`
+  text-transform: uppercase;
+`
