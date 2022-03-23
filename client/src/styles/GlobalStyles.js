e5e378f1be90ddecd000aacf25d79d2f7044e73c
lavalbuena357 <cuervo357@gmail.com>
2022-03-23 12:59:52 -0500

    correccion menor de estilos

--- client/src/styles/GlobalStyles.js
+++ client/src/styles/GlobalStyles.js
@@ -5,6 +5,7 @@ export const GlobalStyles = createGlobalStyle(({ theme }) => ({
     fontFamily: "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
     background: theme.background,
     color: theme.color,
-    transition: 'all 0.3s linear'
+    transition: 'color 0.3s linear, background 0.3s linear',
+    padding: '20px'
   }
 }))
