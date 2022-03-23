46282f81f6196a35959650b66667b182e94b2363
lavalbuena357 <cuervo357@gmail.com>
2022-03-23 12:11:28 -0500

    temas basicos

---
+++ client/src/styles/themes.js
@@ -0,0 +1,11 @@
+export const lightTheme = {
+  background: '#fff',
+  color: '#1d1f28',
+  bottonBg: '#1d1f28'
+}
+
+export const darkTheme = {
+  background: '#1d1f28',
+  color: '#fafafa',
+  bottonBg: '#515d90'
+}
