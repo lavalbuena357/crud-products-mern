4e13789a7d27473c358f3a22b0d1fb54996c91ab
lavalbuena357 <cuervo357@gmail.com>
2022-03-23 12:17:26 -0500

    boton para cambiar tema

---
+++ client/src/Components/ThemeToggler.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import {Button } from '../styles/Button'
+
+
+function ThemeToggler(props) {
+
+  return (
+    <Button onClick={props.changeTheme}>
+      Cambiar tema
+    </Button>
+  )
+}
+
+export default ThemeToggler
