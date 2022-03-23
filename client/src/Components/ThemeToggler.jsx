a70b0d3d22a45b4bbfcf76bb72396ed6a5c4112f
lavalbuena357 <cuervo357@gmail.com>
2022-03-23 13:36:24 -0500

    icono de cambio de tema

--- client/src/Components/ThemeToggler.jsx
+++ client/src/Components/ThemeToggler.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Button } from '../styles/Button'
+import { BsSunFill, BsCloudMoon } from "react-icons/bs"
+import useTheme from '../Hooks/useTheme'
 
 
 function ThemeToggler(props) {
 
   return (
     <Button onClick={props.changeTheme}>
-      Cambiar tema
+      {props.theme === 'light' ? <BsSunFill/> : <BsCloudMoon/> }
     </Button>
   )
 }
