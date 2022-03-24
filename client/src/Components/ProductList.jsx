0465b8e42e4bcf117f30b2ddeb496e7b014212aa
lavalbuena357 <cuervo357@gmail.com>
2022-03-24 14:19:26 -0500

    se creo el componente de productos

--- client/src/Components/ProductList.jsx
+++ client/src/Components/ProductList.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getProducts } from '../redux/actions'
+import { Cards } from '../styles/Styles'
+import Product from './Product'
 
 function ProductList() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  const products = useSelector(state => state.products)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    return (async () => {
+      await dispatch(getProducts())
+      setIsLoading(false)
+    })()
+  }, [])
+
   return (
-    <div>ProductList</div>
+    <Cards>
+      {
+        isLoading ? <p>Cargando...</p>
+        :
+        products.map((el, i) => (
+          <Product 
+            key={i} 
+            image={el.imgUrl} 
+            name={el.name}
+            price={el.unitaryPrice}
+            size={el.size}
+            description={el.description}
+          />
+        ))
+      }
+    </Cards>
   )
 }
 
