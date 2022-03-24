f74fbf08a163e02c5beb883aaff7f25fddfd7517
lavalbuena357 <cuervo357@gmail.com>
2022-03-24 14:17:51 -0500

    card añadido a los temas

--- client/src/styles/themes.js
+++ client/src/styles/themes.js
@@ -1,11 +1,13 @@
 export const lightTheme = {
   background: '#fff',
   color: '#1d1f28',
-  bottonBg: '#1d1f28'
+  bottonBg: '#1d1f28',
+  cardBg: '#515d90'
 }
 
 export const darkTheme = {
   background: '#1d1f28',
   color: '#fafafa',
-  bottonBg: '#515d90'
+  bottonBg: '#515d90',
+  cardBg: '#fafafa'
 }
