519892efa9e9eb544d44591170e9ffacbeaf24e6
lavalbuena357 <cuervo357@gmail.com>
2022-03-24 14:18:32 -0500

    se añadieron componentes de estilo nuevos

--- client/src/styles/Styles.js
+++ client/src/styles/Styles.js
@@ -4,6 +4,8 @@ import styled from "styled-components"
 export const Container = styled.div({
   display: 'flex',
   flexDirection: 'column',
+  justifyContent: 'space-between',
+  minHeight: '100vh'
 })
 
 //BUTTON
@@ -16,19 +18,22 @@ export const Button = styled.button(({theme}) => ({
   fontSize: '1.3rem',
   borderRadius: '25px',
   display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center'
+  "&:hover":  {
+    background: theme.cardBg,
+    color: theme.background
+  }
 }))
 
 export const BoxButton = styled(Button)`
-   border-radius: 5px
+   border-radius: 5px;
 `
 
 //HEADER
 export const HeaderCtn = styled.div({
   display: 'flex',
   alignItems: 'center',
-  justifyContent: 'space-between'
+  justifyContent: 'space-between',
+  padding: '20px 30px',
 })
 
 //TITLE
@@ -36,12 +41,54 @@ export const Title = styled.h1({
   textTransform: 'uppercase'
 })
 
+//HOME CONTAINER
+export const HomeCtn = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'flex-start',
+  margin: '5px 30px',
+  button: {
+    display: 'flex',
+    alignSelf: 'flex-end',
+  }
+})
+
+//CARDS
+export const Cards = styled.div({
+  display: 'flex',
+  justifyContent: 'center',
+  flexWrap: 'wrap',
+  margin: '20px'
+})
+
+//CARD
+export const Card = styled.div(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  maxWidth: '200px',
+  margin: '10px 5px',
+  padding: '10px',
+  background: theme.cardBg,
+  color: theme.background,
+  h2: {
+    fontWeight: '300',
+    margin: '0'
+  },
+  p: {
+    margin: '0'
+  },
+  img: {
+    width: '200px'
+  },
+  div: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    margin: '10px 0'
+  }
+}))
+
 //FOOTER
 export const FooterCtn = styled.div(({ theme }) => ({
-  position: 'absolute',
-  bottom: '0px',
-  left: '0px',
-  right: '0px',
   background: theme.bottonBg,
   color: '#fff',
   display: 'flex',
