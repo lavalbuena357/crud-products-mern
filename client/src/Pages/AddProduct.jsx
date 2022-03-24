b0041cd6fd1a303713c9a0c85057da036b36a078
lavalbuena357 <cuervo357@gmail.com>
2022-03-24 18:37:31 -0500

    mejora para añadir productos

--- client/src/Pages/AddProduct.jsx
+++ client/src/Pages/AddProduct.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from '../Components/Form'
+import { postProduct } from '../redux/actions'
 import { BoxCtn, Subtitle } from '../styles/Styles'
 
 function AddProduct() {
+  const [product, setProduct] = useState({
+    imgUrl: '',
+    name: '',
+    unitaryPrice: 0,
+    size: 0,
+    description: ''
+  })
 
   return (
     <BoxCtn>
       <Subtitle>Add New Product</Subtitle>
-      <Form />
+      <Form 
+        product={product} 
+        setProduct={setProduct} 
+        function={postProduct}  
+        route='/'
+      />
     </BoxCtn>
   )
 }
