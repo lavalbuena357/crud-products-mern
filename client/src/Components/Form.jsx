044d6df6d691968242d5d867fbfded0e4db552af
lavalbuena357 <cuervo357@gmail.com>
2022-03-24 18:37:40 -0500

    formulario reutilizable

--- client/src/Components/Form.jsx
+++ client/src/Components/Form.jsx
@@ -1,44 +1,36 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import { postProduct } from '../redux/actions'
 import { InputText, FormCtn, Label, SubmitBtn, TextArea } from '../styles/Styles'
 
-function Form() {
-  const [product, setProduct] = useState({
-    imgUrl: '',
-    name: '',
-    unitaryPrice: 0,
-    size: 0,
-    description: ''
-  })
+function Form(props) {
 
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
   async function handleSubmit(e) {
     e.preventDefault()
-    await dispatch(postProduct(product))
-    navigate('/')
+    await dispatch(props.function(props.product, props.id))
+    navigate(props.route)
   } 
 
   function handleChange(e) {
     const {name, value} =e.target
-    setProduct({...product, [name]: value})
+    props.setProduct({...props.product, [name]: value})
   }
 
   return (
     <FormCtn onSubmit={handleSubmit}>
       <Label>Image: </Label>
-      <InputText type="text" placeholder='Enter url image...' name='imgUrl' onChange={handleChange} />
+      <InputText type="text" placeholder='Enter url image...' name='imgUrl' onChange={handleChange} value={props.product.imgUrl} />
       <Label>*Name: </Label>
-      <InputText type="text" placeholder='Enter name...' required name='name' onChange={handleChange} />
+      <InputText type="text" placeholder='Enter name...' required name='name' onChange={handleChange} value={props.product.name} />
       <Label>*Unit Price: </Label>
-      <InputText type="number" placeholder='Enter price...' required name='unitaryPrice' onChange={handleChange} />
+      <InputText type="number" placeholder='Enter price...' required name='unitaryPrice' onChange={handleChange} value={props.product.unitaryPrice} />
       <Label>*Amount: </Label>
-      <InputText type="number" placeholder='Enter amount...' required name='size' onChange={handleChange} />
+      <InputText type="number" placeholder='Enter amount...' required name='size' onChange={handleChange} value={props.product.size} />
       <Label>Description: </Label>
-      <TextArea cols="30" rows="5" placeholder='Enter description...' name='description' onChange={handleChange}></TextArea>
+      <TextArea cols="30" rows="5" placeholder='Enter description...' name='description' onChange={handleChange} value={props.product.description}></TextArea>
       <SubmitBtn type="submit" value="Save" />
     </FormCtn>
   )
