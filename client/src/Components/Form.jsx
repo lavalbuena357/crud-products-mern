f2aac283d931aeb616dfb891cadddc5ad2bdab4c
lavalbuena357 <cuervo357@gmail.com>
2022-03-24 17:00:52 -0500

    formulario para agregar producto

--- client/src/Components/Form.jsx
+++ client/src/Components/Form.jsx
@@ -1,25 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { postProduct } from '../redux/actions'
 import { InputText, FormCtn, Label, SubmitBtn, TextArea } from '../styles/Styles'
 
 function Form() {
+  const [product, setProduct] = useState({
+    imgUrl: '',
+    name: '',
+    unitaryPrice: 0,
+    size: 0,
+    description: ''
+  })
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   async function handleSubmit(e) {
-    e.preventDefault(e)
-    console.log('submit')
+    e.preventDefault()
+    await dispatch(postProduct(product))
+    navigate('/')
   } 
+
+  function handleChange(e) {
+    const {name, value} =e.target
+    setProduct({...product, [name]: value})
+  }
 
   return (
     <FormCtn onSubmit={handleSubmit}>
       <Label>Image: </Label>
-      <InputText type="text" placeholder='Enter url image...' />
-      <Label>Name: </Label>
-      <InputText type="text" placeholder='Enter name...' />
-      <Label>Unit Price: </Label>
-      <InputText type="number" placeholder='Enter price...' />
-      <Label>Amount: </Label>
-      <InputText type="number" placeholder='Enter amount...' />
+      <InputText type="text" placeholder='Enter url image...' name='imgUrl' onChange={handleChange} />
+      <Label>*Name: </Label>
+      <InputText type="text" placeholder='Enter name...' required name='name' onChange={handleChange} />
+      <Label>*Unit Price: </Label>
+      <InputText type="number" placeholder='Enter price...' required name='unitaryPrice' onChange={handleChange} />
+      <Label>*Amount: </Label>
+      <InputText type="number" placeholder='Enter amount...' required name='size' onChange={handleChange} />
       <Label>Description: </Label>
-      <TextArea name="" id="" cols="30" rows="10" placeholder='Enter description...'></TextArea>
+      <TextArea cols="30" rows="5" placeholder='Enter description...' name='description' onChange={handleChange}></TextArea>
       <SubmitBtn type="submit" value="Save" />
     </FormCtn>
   )
