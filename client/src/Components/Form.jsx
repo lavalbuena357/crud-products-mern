ec5cdd603db460a1ae0ff86f603308acf4fc2e6c
lavalbuena357 <cuervo357@gmail.com>
2022-03-24 16:35:50 -0500

    componente formulario

--- client/src/Components/Form.jsx
+++ client/src/Components/Form.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { InputText, FormCtn, Label, SubmitBtn, TextArea } from '../styles/Styles'
 
 function Form() {
+
+  async function handleSubmit(e) {
+    e.preventDefault(e)
+    console.log('submit')
+  } 
+
   return (
-    <div>Form</div>
+    <FormCtn onSubmit={handleSubmit}>
+      <Label>Image: </Label>
+      <InputText type="text" placeholder='Enter url image...' />
+      <Label>Name: </Label>
+      <InputText type="text" placeholder='Enter name...' />
+      <Label>Unit Price: </Label>
+      <InputText type="number" placeholder='Enter price...' />
+      <Label>Amount: </Label>
+      <InputText type="number" placeholder='Enter amount...' />
+      <Label>Description: </Label>
+      <TextArea name="" id="" cols="30" rows="10" placeholder='Enter description...'></TextArea>
+      <SubmitBtn type="submit" value="Save" />
+    </FormCtn>
   )
 }
 
