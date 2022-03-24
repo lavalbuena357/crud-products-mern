ab9b1b874971b3cb9f4367e8b9c26237c6120da3
lavalbuena357 <cuervo357@gmail.com>
2022-03-24 17:00:38 -0500

    componentes de estilo

--- client/src/Components/Product.jsx
+++ client/src/Components/Product.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
-import { BoxButton, Card } from '../styles/Styles'
+import { BoxButton, Card, BoxButtons, BoxInfo } from '../styles/Styles'
 import { BsEyeFill, BsFillTrashFill, BsPencilSquare } from "react-icons/bs"
 
 function Product(props) {
   return (
     <Card>
-      <img src={props.image} alt="imagen" />
-      <h2>{props.name}</h2>
-      <p>Price: ${props.price} COP</p>
-      <p>Amount: {props.size}</p>
-      <p>Description: {props.description}</p>
-      <div>
+      <BoxInfo>
+        <img src={props.image} alt="imagen" />
+        <h2>{props.name}</h2>
+        <p>Price: ${props.price} COP</p>
+        <p>Amount: {props.size}</p>
+        <p>Description: {props.description}</p>
+      </BoxInfo>
+      <BoxButtons>
         <BoxButton><BsEyeFill /></BoxButton>
         <BoxButton><BsPencilSquare /></BoxButton>
         <BoxButton><BsFillTrashFill /></BoxButton>
-      </div>
+      </BoxButtons>
     </Card>
   )
 }
