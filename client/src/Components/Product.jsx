96b48257b5fdc4b8543944dad501943912b3ca42
lavalbuena357 <cuervo357@gmail.com>
2022-03-25 12:25:22 -0500

    cambios menor de estilos

--- client/src/Components/Product.jsx
+++ client/src/Components/Product.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BoxButton, Card, BoxButtons, BoxInfo } from '../styles/Styles'
+import { BoxButton, Card, BoxButtons, BoxInfo, BoxImage } from '../styles/Styles'
 import { BsEyeFill, BsFillTrashFill, BsPencilSquare } from "react-icons/bs"
 import { useDispatch } from 'react-redux'
 import { deleteProduct } from '../redux/actions'
@@ -24,18 +24,24 @@ function Product(props) {
 
   return (
     <Card>
-      <BoxInfo>
-        <img src={props.image} alt="imagen" />
-        <h2>{props.name}</h2>
-        <p>Price: ${props.price} COP</p>
-        <p>Amount: {props.size}</p>
-        <p>Description: {props.description}</p>
-      </BoxInfo>
-      <BoxButtons>
-        <BoxButton onClick={() => handleDetail(props.id)}><BsEyeFill /></BoxButton>
-        <BoxButton onClick={() => handleUpdate(props.id)}><BsPencilSquare /></BoxButton>
-        <BoxButton onClick={() => handleDelete(props.id)}><BsFillTrashFill /></BoxButton>
-      </BoxButtons>
+      <div>
+        <BoxImage>
+          <img src={props.image} alt="imagen" />
+        </BoxImage>
+      </div>
+      <div>
+        <BoxInfo> 
+          <h2>{props.name}</h2>
+          <p>Price: ${props.price} COP</p>
+          <p>Amount: {props.size}</p>
+          <p>Description: {props.description}</p>
+        </BoxInfo>
+        <BoxButtons>
+          <BoxButton onClick={() => handleDetail(props.id)}><BsEyeFill /></BoxButton>
+          <BoxButton onClick={() => handleUpdate(props.id)}><BsPencilSquare /></BoxButton>
+          <BoxButton onClick={() => handleDelete(props.id)}><BsFillTrashFill /></BoxButton>
+        </BoxButtons>
+      </div>
     </Card>
   )
 }
