9692bb080ebe5fd6247585b934b9d38604ee4138
lavalbuena357 <cuervo357@gmail.com>
2022-03-25 13:10:58 -0500

    componente detail avance

--- client/src/Pages/Detail.jsx
+++ client/src/Pages/Detail.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { getProduct } from '../redux/actions'
+import { BoxCtn, BoxImage, BoxInfo, DetailBox } from '../styles/Styles'
 
 function Detail() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  const {id} = useParams()
+  const dispatch = useDispatch()
+  const detail = useSelector(state => state.detail)
+
+  useEffect(() => {
+    return (async () => {
+      await dispatch(getProduct(id))
+      setIsLoading(false)
+    })()
+  }, [])
+
   return (
-    <div>Detail</div>
+    <BoxCtn>
+      {isLoading ? <p>Loading...</p>
+      :
+      <DetailBox>
+        <BoxImage imgSize='400'>
+          <img src={detail.imgUrl} alt="" />
+        </BoxImage>
+        <BoxInfo align='flex-start'>
+          <h2>{detail.name}</h2>
+          <p>Price: ${detail.price} COP</p>
+          <p>Amount: {detail.size}</p>
+          <p>Description: {detail.description}</p>
+          <p>Full Description:</p>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas suscipit laudantium sed dolore natus autem repellat, reiciendis perferendis magnam expedita ad neque ex dicta, veritatis sunt velit in, commodi ullam.</p>
+        </BoxInfo>
+      </DetailBox>
+      }
+    </BoxCtn>
   )
 }
 
